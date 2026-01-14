03b06d555c03ebcd89ba03ff61f184b2973dd76a
Tenkey25 <sho.matsumura1012@gmail.com>
2026-01-14 17:20:50 +0900

    feat(tasks): implement task store

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Models\Project;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $projects = Project::query()
+            ->orderBy('id', 'desc')
+            ->get(['id', 'name']);
+
+        //DBにプロジェクトが未登録
+        if ($projects->isEmpty()) {
+            return redirect()
+                ->route('projects.create')
+                ->with('success', '先にプロジェクトを登録してください。');
+        }
+
+        return view('tasks.create', compact('projects'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        Task::create($request->validated());
+
+        return redirect()
+            ->route('projects.index')
+            ->with('success', 'タスクを登録しました。');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'project_id' => ['required', 'exists:projects,id'],
+            'title' => ['required', 'string', 'max:200'],
+            'description' => ['nullable', 'string', 'max:2000'],
+            'status' => ['required', 'in:todo,doing,done'],
+            'due_date' => ['nullable', 'date'],
+        ];
+    }
+}
